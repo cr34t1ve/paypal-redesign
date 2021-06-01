4047d7ce261737a873e3537bec7aa3bb05e8c6a1
cr34t1ve <cr34t1ve@protonmail.com>
2021-06-01 16:44:11 +0000

    'login' page initialised

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:paypal_redesign/screens/home/home.dart';
+import 'package:paypal_redesign/screens/login/login.dart';
 import 'package:paypal_redesign/screens/wallet/wallet.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    home: Login(),
   ));
 }
