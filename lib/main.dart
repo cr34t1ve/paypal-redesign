ab3121668b78704d981137528061ffeb52d159f9
cr34t1ve <cr34t1ve@protonmail.com>
2021-06-04 15:07:57 +0000

    added navigation routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:paypal_redesign/screens/activity/activity.dart';
 import 'package:paypal_redesign/screens/contacts/contacts.dart'; 
-import 'package:paypal_redesign/screens/home/home.dart';
+// import 'package:paypal_redesign/screens/home/home.dart';
 import 'package:paypal_redesign/screens/login/login.dart';
 import 'package:paypal_redesign/screens/navpage/navpage.dart';
 import 'package:paypal_redesign/screens/sendmoney/sendmoney.dart';
-import 'package:paypal_redesign/screens/wallet/wallet.dart';
+// import 'package:paypal_redesign/screens/wallet/wallet.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Contacts(),
     debugShowCheckedModeBanner: false,
+    routes: {
+      '/': (context) => Login(),
+      '/navpage': (context) => NavPage(),
+      '/activity': (context) => Activity(),
+      '/sendmoney': (context) => SendMoney(paypalContacts: null),
+    },
   ));
 }
