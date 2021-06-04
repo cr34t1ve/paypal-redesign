284e24d6623b2beb6894e7cb7739b4fc924058ee
cr34t1ve <cr34t1ve@protonmail.com>
2021-06-04 15:43:45 +0000

    status bar set to transparent

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:paypal_redesign/screens/activity/activity.dart';
-import 'package:paypal_redesign/screens/contacts/contacts.dart'; 
-// import 'package:paypal_redesign/screens/home/home.dart';
 import 'package:paypal_redesign/screens/login/login.dart';
 import 'package:paypal_redesign/screens/navpage/navpage.dart';
 import 'package:paypal_redesign/screens/sendmoney/sendmoney.dart';
-// import 'package:paypal_redesign/screens/wallet/wallet.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+ ));
   runApp(MaterialApp(
-    // home: Contacts(),
     debugShowCheckedModeBanner: false,
     initialRoute: '/',
     routes: {
